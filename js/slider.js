4d9d4fd190f6ef56568956b61014061270b23acb
Daniel Kunes <dkunes87@gmail.com>
2013-04-06 20:30:01 +0200

    initial animation of slider

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+function animateSlider(){
+	var guiImg = $('#obr1');
+	var mappingImg = $('#obr2');
+	var exportImg = $('#obr3');
+
+	var browserWidth = $(window).width();
+	console.log("browserWidth: " + browserWidth);
+
+	guiImg.margin();
+
+}

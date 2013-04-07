c685b16a7ae38a456ee331231be0bcdf93b7a30a
Antonin Slezacek <slezicz@gmail.com>
2013-04-07 02:27:52 +0200

    SUPER

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,8 @@ function animateSlider(){
 	var guiImg = $('#obr1');
 	var mappingImg = $('#obr2');
 	var exportImg = $('#obr3');
+	var arrow1 = $('#arr1');
+	var arrow2 = $('#arr2');
 
 	var browserWidth = $(window).width();
 	console.log("browserWidth: " + browserWidth);
@@ -20,6 +22,8 @@ function animateSlider(){
 	var mappingHeight = mappingImg.height();
 	var exportWidth = exportImg.width();
 	var exportHeight = exportImg.height();
+	var arrWidth = arrow1.width();
+	var arrHeight = arrow1.height();
 
 	guiImg.width(0);
 	guiImg.height(0);
@@ -27,11 +31,16 @@ function animateSlider(){
 	mappingImg.height(0);
 	exportImg.width(0);
 	exportImg.height(0);
+	arrow1.height(0);
+	arrow1.width(0);
+	arrow2.width(0);
+	arrow2.height(0);
 
 	guiImg.css({left: startPosition});
 	mappingImg.css({left: startPosition});
 	exportImg.css({left: startPosition});
-
+	arrow1.css({left: startPosition});
+	arrow2.css({left:startPosition});
 
 	guiImg.animate({
 		left: endPosition,
@@ -39,19 +48,29 @@ function animateSlider(){
 		height: guiHeight
 
 	}, 1000,  "swing", function(){
-
-		mappingImg.animate({
+		arrow1.animate({
 			left: endPosition,
-			width: mappingWidth,
-			height: mappingHeight
-		}, 1000,  "swing", function(){
-
-			exportImg.animate({
+			width: arrWidth,
+			height: arrHeight
+		}, 500, "swing", function(){
+			mappingImg.animate({
 				left: endPosition,
-				width: exportWidth,
-				height: exportHeight
-			}, 1000,  "swing",function() {
-				
+				width: mappingWidth,
+				height: mappingHeight
+			}, 1000,  "swing", function(){
+				arrow2.animate({
+					left: endPosition,
+					width: arrWidth,
+					height: arrHeight
+				}, 500, "swing", function(){
+					exportImg.animate({
+						left: endPosition,
+						width: exportWidth,
+						height: exportHeight
+					}, 1000,  "swing",function() {
+
+					});
+				});
 			});
 		});
 	});
